8dd19cad6e9885bfb16f402fc75d4ea97503f23e
xieweizeng <xieweizeng@gmail.com>
2020-10-04 18:05:18 +0800

    增加了标题对齐功能

--- TitleProcess.py
+++ TitleProcess.py
@@ -1,11 +1,13 @@
 # 程序功能：从文本文档中提取CCNA在线课件的章节页编号与标题，并保存到另一个文本文件中（用于下一步导入到Excel当中）
-
+# TODO:
+# 应添加关于文件处理的异常处理代码
 
 import re
 
-SourceFileName = "TitleList.txt"
-UsefulFileName = "UsefulTitleList.txt"
-UsefulContents = []
+SourceFileName = "TitleList.txt"        # 原始数据所在文件名
+UsefulFileName = "UsefulTitleList.txt"  # 处理后的数据要写入的文件名
+UsefulContents = []                     # 用于暂存要写入的内容
+MaxTabCount = 4                         # 每行应该至少有4个\t
 
 
 # 函数功能：接收1个字符串，功能是判断字符串是否能够匹配正则表达式，返回值为布尔值
@@ -18,12 +20,40 @@ def check_line(line):
         return False
 
 
-# 函数功能：接收1个字符串，功能是按照特定规则截取字符串的部分内容拼接成符合需要的新字符串，并返回该字符串
+# 函数功能：接收1个字符串，功能是按照特定规则（以双引号为分隔符）截取字符串的部分内容拼接成符合需要的新字符串，并返回该字符串
 def cutdown(line):
     parts = line.split('"')
     newline = parts[1] + '\t' + parts[3] + '\n'
-    newline = newline.replace('.', '\t\t')
+    newline = newline.replace('.', '\t')
     return newline
+
+# 函数功能：如果章节标题达不到足够的长度，补足相应的\t
+
+
+def addup(line):
+    count = 0
+    for char in line:   # 计算当前行中所包含的 \t 的个数
+        if char == '\t':
+            count += 1
+    if count == MaxTabCount:    # 如果当前行中所包含的 \t 的个数已经达到最大值，则直接返回该行
+        return line
+    if count < MaxTabCount:     # 如果当前行中所包含的 \t 的个数＜最大值，则需要补充\t
+        parts = line.split('\t')
+        length = len(parts)
+        i = 0
+        while i <= count-1:     # 在数字编号之间加上\t
+            parts[i] += '\t'
+            i += 1
+        j = 1
+        while j <= MaxTabCount - i:     # 在标题之前加入足够的\t
+            parts.insert(-1, '\t')
+            j += 1
+        newline = ''
+        for part in parts:
+            newline += part
+        return newline
+    else:
+        return ''       # 此处应返回异常
 
 
 with open(SourceFileName, 'r', encoding="(utf8)") as file_object:
@@ -33,6 +63,7 @@ for i in range(len(contents)):
     if check_line(contents[i]):
         NewLine = contents[i]
         NewLine = cutdown(NewLine)
+        NewLine = addup(NewLine)
         UsefulContents.append(NewLine)
 
 print('一共有 {} 行Useful Line 将被写入到新文件！'.format(str(len(UsefulContents))))
@@ -41,4 +72,5 @@ with open(UsefulFileName, 'w', encoding="(utf8)") as file_object:
     for i in range(len(UsefulContents)):
         file_object.write(UsefulContents[i])
 
-print('写入操作完成，已经把 {} 行数据写入到文件 {} 中。'.format(str(len(UsefulContents)), UsefulFileName))
+print('写入操作完成，已经把 {} 行数据写入到文件 {} 中。'.format(
+    str(len(UsefulContents)), UsefulFileName))
